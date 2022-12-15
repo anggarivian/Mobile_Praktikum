65ab869f0dfa529e6eedaa270505f15903ee4984
anggarivian <anggarivian.11@gmail.com>
2022-12-15 14:44:02 +0700

    Mobile 4

---
+++ app/src/main/java/com/gogo/mobile_4/TambahActivity.kt
@@ -0,0 +1,11 @@
+package com.gogo.mobile_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TambahActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah)
+    }
+}
