6a3002ad03c95f91e181e95060548cc105745297
anggarivian <anggarivian.11@gmail.com>
2022-12-15 14:56:39 +0700

    Mobile

--- app/src/main/java/com/gogo/mobile_4/DataActivity.kt
+++ app/src/main/java/com/gogo/mobile_4/DataActivity.kt
@@ -1,11 +1,52 @@
 package com.gogo.mobile_4
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.gogo.mobile_4.databinding.ActivityDataBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
-class DataActivity : AppCompatActivity() {
+class ActivityData : AppCompatActivity() {
+    lateinit var binding: ActivityDataBinding
+    lateinit var db: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityDataBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_data)
+        setContentView(binding.root)
+
+        db = FirebaseFirestore.getInstance()
+        db.collection("kredits").document(intent.getStringExtra("idDoc").toString())
+            .get()
+            .addOnSuccessListener {
+                binding.edtNominal.setText(it.data?.get("Nominal").toString())
+                binding.edtTenor.setText(it.data?.get("Tenor").toString())
+                binding.edtAngsuran.setText(it.data?.get("Angsuran").toString())
+                it.data?.get("").toString()
+            }
+            .addOnFailureListener{
+                it.printStackTrace()
+                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+            }
+
+        binding.btnDelete.setOnClickListener {
+            delete()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun delete(){
+        db.collection("kredits")
+            .document(intent.getStringExtra("idDoc").toString())
+            .delete()
+            .addOnSuccessListener { Log.d(TAG, "Doc Succesfully Deteled!")
+                Toast.makeText(this, "Delete Berhasil", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e -> Log.w(TAG, "Deleting Error", e)
+                Toast.makeText(this, "Gagal Delete", Toast.LENGTH_SHORT).show()
+            }
     }
 }
